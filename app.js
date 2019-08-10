375b397a9ad3b0ff4878fbc3e6d4b562a50f7e9c
jhonny perez <jhonnyperezmo@gmail.com>
2019-08-09 23:41:40 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 
 // Inicializar variables
 var app = express();
+
+
+//body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Importar Rutas
+var appRoute = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var LoginRoutes = require('./routes/login');
 
 mongoose.connection.openUri('mongodb://127.0.0.1:27017/hospitalDB', { useNewUrlParser: true },
     (err, res) => {
@@ -14,14 +25,9 @@ mongoose.connection.openUri('mongodb://127.0.0.1:27017/hospitalDB', { useNewUrlP
     });
 
 //rutas
-app.get('/', (req,res,next) =>{
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-
-})
+app.use('/usuario',usuarioRoutes);
+app.use('/login',LoginRoutes);
+app.use('/',appRoute);
 
 
 // escuchar peticones
